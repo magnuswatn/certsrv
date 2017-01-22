16e7b133029ca827db2039097e587a2f7223dab9
magnuswatn <magnus@watn.no>
2017-01-22 13:49:38 +0100

    Updated setup.py for PyPI release

--- setup.py
+++ setup.py
@@ -3,14 +3,25 @@ try:
 except ImportError:
     from distutils.core import setup
 
-config = {
-    'description': 'A Python client for the Microsoft AD Certificate Services web page',
-    'author': 'Magnus Watn',
-    'url': 'https://github.com/magnuswatn/certsrv',
-    'download_url': 'https://github.com/magnuswatn/certsrv',
-    'version': '0.9',
-    'py_modules': ['certsrv'],
-    'name': 'certsrv'
-}
-
-setup(**config)
+setup(
+    name='certsrv',
+    description='A Python client for the Microsoft AD Certificate Services web page',
+    author='Magnus Watn',
+    license='MIT',
+    url='https://github.com/magnuswatn/certsrv',
+    keywords='ad adcs certsrv pki certificate',
+    version='1.0.0',
+    py_modules=['certsrv'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Security',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: System :: Systems Administration',
+        ],
+)
